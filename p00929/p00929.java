01b7c0c5e02504161ca540c99425e1627b6bb30b
vutoan1245 <vutoan1245@gmail.com>
2019-08-07 18:49:37 -0400

    add solution for problem 929

--- p00929/p00929.java
+++ p00929/p00929.java
@@ -1,48 +1,84 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-class p00929{
-    public static void main(String args[]) throws Exception{
+class p00929 {
+    static final int POS_INF = 1000 * 1000 * 10;
+
+    public static void main(String args[]) throws NumberFormatException, IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         int caseNum = Integer.parseInt(in.readLine());
-        for(int i = 0; i < caseNum; i++){
+
+        String strArr[];
+        while (caseNum-- > 0) {
             int rowNum = Integer.parseInt(in.readLine());
             int colNum = Integer.parseInt(in.readLine());
-            int graph[][] = new int[rowNum][colNum];
-            int result[][] = new int[rowNum][colNum];
-            for(int j = 0; j < rowNum; j++){
-                String strArr[] = in.readLine().split(" ");
-                for(int k = 0; k < colNum; k++){
-                    graph[j][k] = Integer.parseInt(strArr[k]);
-                    result[j][k] = Integer.MAX_VALUE;
+
+            int matrix[][] = new int[rowNum][colNum];
+            int currCost[][] = new int[rowNum][colNum];
+            for (int i = 0; i < rowNum; i++) {
+                strArr = in.readLine().split(" ");
+                for (int j = 0; j < colNum; j++) {
+                    matrix[i][j] = Integer.parseInt(strArr[j]);
+                    currCost[i][j] = POS_INF;
+
                 }
             }
 
-            // Calculate output
-            int[] dx = new int[]{-1, 0, 0, 1};
-            int[] dy = new int[]{0, -1, 1, 0};
-            result[0][0] = graph[0][0];
+            currCost[0][0] = matrix[0][0];
 
             PriorityQueue<Node> pq = new PriorityQueue<Node>();
-            pq.offer(new Node(0, 0, graph[0][0]));
+            pq.add(new Node(0, 0, currCost[0][0]));
 
+            int result = 0;
             while (!pq.isEmpty()) {
-                Node node = pq.poll();
-                if (node.row == rowNum - 1 && node.col == colNum - 1) {
-                    System.out.println(result[rowNum - 1][colNum - 1]);
+                Node n = pq.poll();
+                if (n.i == rowNum - 1 && n.j == colNum - 1) {
+                    result = n.cost;
                     break;
                 }
-                for (int k = 0; k < 4; k++) {
-                    int r = node.row + dx[k];
-                    int c = node.col + dy[k];
-                    if (r >= 0 && r < rowNum && c >= 0 && c < colNum && graph[r][c] + result[node.row][node.col] < result[r][c]) {
-                        result[r][c] = graph[r][c] + result[node.row][node.col];
-                        pq.add(new Node(r, c, result[r][c]));
+
+                // Top
+                if (n.i - 1 >= 0) {
+                    int i = n.i - 1;
+                    int j = n.j;
+                    if (n.cost + matrix[i][j] < currCost[i][j]) {
+                        currCost[i][j] = n.cost + matrix[i][j];
+                        pq.add(new Node(i, j, currCost[i][j]));
+                    }
+                }
+                // Right
+                if (n.j + 1 < colNum) {
+                    int i = n.i;
+                    int j = n.j + 1;
+                    if (n.cost + matrix[i][j] < currCost[i][j]) {
+                        currCost[i][j] = n.cost + matrix[i][j];
+                        pq.add(new Node(i, j, currCost[i][j]));
+                    }
+                }
+                // Bottom
+                if (n.i + 1 < rowNum) {
+                    int i = n.i + 1;
+                    int j = n.j;
+                    if (n.cost + matrix[i][j] < currCost[i][j]) {
+                        currCost[i][j] = n.cost + matrix[i][j];
+                        pq.add(new Node(i, j, currCost[i][j]));
+                    }
+                }
+                // Left
+                if (n.j - 1 >= 0) {
+                    int i = n.i;
+                    int j = n.j - 1;
+                    if (n.cost + matrix[i][j] < currCost[i][j]) {
+                        currCost[i][j] = n.cost + matrix[i][j];
+                        pq.add(new Node(i, j, currCost[i][j]));
                     }
                 }
             }
+
+            System.out.println(result);
         }
 
         in.close();
@@ -50,14 +86,14 @@ class p00929{
 }
 
 class Node implements Comparable<Node> {
-    int cost, row, col;
-    
-    public Node(int row, int col, int cost) {
-        this.row = row;
-        this.col = col;
+    int i, j, cost;
+
+    Node(int i, int j, int cost) {
+        this.i = i;
+        this.j = j;
         this.cost = cost;
     }
-    
+
     public int compareTo(Node o) {
         return cost - o.cost;
     }
