71797462479404e4ad8dbde82a481961ad77c835
Toan Vu <vutoan1245@gmail.com>
2019-08-25 17:01:41 -0400

    add solution for problem 10130

--- p10130/p10130.java
+++ p10130/p10130.java
@@ -1,43 +1,44 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-class p10130{
-    public static void main(String args[]){
+class p10130 {
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
         int caseNum = in.nextInt();
-        while(caseNum-->0){
+        while (caseNum-- > 0) {
+
+            // Get input
             int itemNum = in.nextInt();
             Item itemList[] = new Item[itemNum];
-            for(int i = 0; i < itemNum; i++){
+            for (int i = 0; i < itemNum; i++) {
                 itemList[i] = new Item(in.nextInt(), in.nextInt());
             }
-            Arrays.sort(itemList);
 
             int peopleNum = in.nextInt();
-            int weightList[] = new int[peopleNum];
-            for(int i = 0; i < peopleNum; i++){
-                weightList[i] = in.nextInt();
+            int limit[] = new int[peopleNum];
+            int max = 0;
+            for (int i = 0; i < peopleNum; i++) {
+                limit[i] = in.nextInt();
+                max = Math.max(max, limit[i]);
             }
-            Arrays.sort(weightList);
 
-            // Calculate output
-            int result = 0;
-            for(int i = 0; i < peopleNum; i++){
-                int weight = weightList[i];
-                int dp[][] = new int[itemNum+1][weight+1];
-                
-                for(int j = 1; j <= itemNum; j++){
-                    for(int k = 1; k <= weight; k++){
-                        if(k < itemList[j-1].weight){
-                            dp[j][k] = dp[j-1][k];
-                        } else {
-                            dp[j][k] = Math.max(itemList[j-1].value + dp[j-1][k-itemList[j-1].weight], dp[j-1][k]);
-                        }
+            // Calculate all possible answers
+            int dp[][] = new int[itemNum + 1][max + 1];
+
+            for (int j = 1; j <= itemNum; j++) {
+                Item currItem = itemList[j - 1];
+                for (int k = 1; k <= max; k++) {
+                    if (k < currItem.weight) {
+                        dp[j][k] = dp[j - 1][k];
+                    } else {
+                        dp[j][k] = Math.max(currItem.value + dp[j - 1][k - currItem.weight], dp[j - 1][k]);
                     }
                 }
+            }
 
-                result += dp[itemNum][weight];
+            int result = 0;
+            for (int i = 0; i < peopleNum; i++) {
+                result += dp[itemNum][limit[i]];
             }
 
             System.out.println(result);
@@ -47,17 +48,11 @@ class p10130{
     }
 }
 
-class Item implements Comparable<Item>{
-    int value;
-    int weight;
-    Item(int value, int weight){
+class Item {
+    int value, weight;
+
+    Item(int value, int weight) {
         this.value = value;
         this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Item other) {
-        if(value == other.value) return weight - other.weight;
-        return value - other.value;
     }
 }
